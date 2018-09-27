3e3b2c01bc3c59192c96bdcb3a216bac2b38a35b
Nathaliemug <42577410+Nathaliemug@users.noreply.github.com>
2018-09-27 16:02:06 +0200

    MODIF EDS

---
+++ JS/js.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $(".container-fluid .navigator .ecole-solidaire").addClass("animate");
+    $(".mobile-icon").click(function () {
+        $(".main-menu").toggle();
+        $(".mobile-icon").toggleClass("fa-bars fa-times");
+        $(".para1").css("left");
+
+        $(".mobile-icon").click(function () {
+            $(".menu").toggle();
+            $(".mobile-icon").toggleClass("fa-bars fa-times");
+        });
+    });
+});
+
+
